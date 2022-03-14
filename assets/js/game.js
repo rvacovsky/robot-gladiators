332cba3b9c8eef2cefff3e4711b0f5693225c178
Roger Vacovsky <rvacovsky@gmail.com>
2022-03-13 21:33:10 -0400

    bug fix blank and null user input

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
+      
       // pick new enemy to fight based on the index of the enemyInfo array
       var pickedEnemyObj = enemyInfo[i];
 
@@ -171,6 +171,7 @@ var shop = function() {
 
 // player information
 var playerInfo = {
+  name: getPlayerName(),
   name: window.prompt("What is your robot's name?"),
   health: 100,
   attack: 10,
@@ -180,6 +181,15 @@ var playerInfo = {
     this.money = 10;
     this.attack = 10;
   },
+  // function to set name
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
   refillHealth: function() {
     if (this.money >= 7) {
       window.alert("Refilling player's health by 20 for 7 dollars.");
